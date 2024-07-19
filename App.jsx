6bbc7aafc41c3118826be711231427f6335110ff
Rohit Samnani <rohitsamnani.sr@gmail.com>
2024-07-19 09:45:50 +0530

    Daily Schedule

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import Navigation from './app/navigation/navigation';
 import { PaperProvider } from 'react-native-paper';
+import { enGB, registerTranslation } from 'react-native-paper-dates';
 
 function App() {
+  registerTranslation('en', enGB)
   return (
     <PaperProvider>
       <Navigation />
---
+++ app/screens/CreateNewSchedule.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { DatePickerInput } from 'react-native-paper-dates';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {TextInput, Button} from 'react-native-paper';
+export default function CreateNewSchedule({ navigation }) {
+
+    const [inputDate, setInputDate] = React.useState(undefined)
+
+    return (
+        <SafeAreaProvider>
+        <View style={{ width:'80%',marginTop:'10%',marginLeft:'2%'  }}>
+          <DatePickerInput
+            locale="en"
+            label="Date"
+            value={inputDate}
+            onChange={(d) => setInputDate(d)}
+            inputMode="start"
+            presentationStyle='formSheet'
+          />
+        </View>
+        <View style={{marginTop:'20%'}}>
+            <TextInput right={<TextInput.Icon icon="eye" />}/>
+        </View>
+      </SafeAreaProvider>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'top',
+        backgroundColor: '#fff',
+
+    },
+
+});
