a863292cb054f9a5f83c88494d7138118b0e9882
Rohit Samnani <aaratrikasen@gmail.com>
2024-07-19 13:45:42 +0530

    Changes related to scheduling

--- app/screens/CreateNewSchedule.jsx
+++ app/screens/CreateNewSchedule.jsx
@@ -1,28 +1,81 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { DatePickerInput } from 'react-native-paper-dates';
+import { DatePickerInput, TimePickerModal } from 'react-native-paper-dates';
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import {TextInput, Button} from 'react-native-paper';
+import { TextInput, Button, IconButton, MD3Colors } from 'react-native-paper';
 export default function CreateNewSchedule({ navigation }) {
 
-    const [inputDate, setInputDate] = React.useState(undefined)
+    const [inputDate, setInputDate] = React.useState(undefined);
+    const [visible, setVisible] = React.useState(false);
+    const [time, setTime] = React.useState('');
+    const [showComment, setShowComment] = React.useState(false);
+
+
+    const onDismiss = React.useCallback(() => {
+        setVisible(false)
+    }, [setVisible])
+
+    const onConfirm = React.useCallback(
+        ({ hours, minutes }) => {
+            setVisible(false);
+            setTime(hours + ':' + minutes)
+        },
+        [setVisible]
+    );
 
     return (
         <SafeAreaProvider>
-        <View style={{ width:'80%',marginTop:'10%',marginLeft:'2%'  }}>
-          <DatePickerInput
-            locale="en"
-            label="Date"
-            value={inputDate}
-            onChange={(d) => setInputDate(d)}
-            inputMode="start"
-            presentationStyle='formSheet'
-          />
-        </View>
-        <View style={{marginTop:'20%'}}>
-            <TextInput right={<TextInput.Icon icon="eye" />}/>
-        </View>
-      </SafeAreaProvider>
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ width: '40%', marginTop: '10%', marginLeft: '2%', flexDirection: 'row' }}>
+
+                    <DatePickerInput
+                        locale="en"
+                        label="Start date"
+                        value={inputDate}
+                        onChange={(d) => setInputDate(d)}
+                        inputMode="end"
+                    />
+                </View>
+                <View style={{ width: '40%', marginTop: '10%', marginLeft: '2%', flexDirection: 'row' }}>
+
+                    <DatePickerInput
+                        locale="en"
+                        label="End date"
+                        value={inputDate}
+                        onChange={(d) => setInputDate(d)}
+                        inputMode="end"
+                    />
+                </View>
+            </View>
+            <View style={{ marginTop: '10%', marginLeft: '2%', flexDirection: 'row' }}>
+                <TextInput label='Task' style={{ width: '40%' }} right={<TextInput.Icon icon="pencil" />} />
+                <TextInput label='Pick Time' value={time} style={{ width: '30%', marginLeft: '2%' }} right={<TextInput.Icon icon="timetable" onPress={() => setVisible(true)} />} />
+                <TimePickerModal
+                    visible={visible}
+                    onDismiss={onDismiss}
+                    onConfirm={onConfirm}
+                    hours={12}
+                    minutes={14}
+                    use24HourClock={true}
+                />
+                <IconButton icon="comment" iconColor={MD3Colors.primary0} onPress={()=>{setShowComment(true)}} />
+            </View>
+            {showComment && <View>
+                <TextInput label='Comment' value={time} style={{ width: '80%', marginLeft: '2%', marginTop: '5%' }} />
+            </View>}
+
+            <View>
+                <Button style={{ marginTop: '10%', width: '50%' }} icon="plus" mode="elevated" >
+                    Add New Task
+                </Button>
+            </View>
+
+            <View style={{ alignSelf: 'flex-end', position: 'absolute', bottom: 40, flexDirection: 'row' }}>
+                <Button style={{ width: '80%' }} mode="elevated" onPress={() =>  navigation.navigate('DailySchedule')}>
+                    Save
+                </Button>
+            </View>
+        </SafeAreaProvider>
 
     );
 }
--- app/screens/DailySchedule.jsx
+++ app/screens/DailySchedule.jsx
@@ -1,66 +1,84 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { List, Button, Checkbox, FAB } from 'react-native-paper';
+import { List, Button, Checkbox, FAB, IconButton } from 'react-native-paper';
 
 export default function DailySchedule({ navigation }) {
 
 
     const [checked, setChecked] = React.useState(false);
+    const [selectedDay, setSelectedDay] = React.useState('19');
+    const [selected, setSelected] = React.useState('Fri');
+    const [end, setEnd] = React.useState('th');
+    
 
     return (
-        <View style={{flex:1}}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingTop:20 }}>
+        <View style={{ flex: 1 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingTop: 20 }}>
                 <FAB
                     label='19'
                     style={styles.fab}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() =>[setSelectedDay('19'), setSelected('Fri'), setEnd('th')]}
                 />
                 <FAB
                     label='20'
                     style={styles.fab}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() => [setSelectedDay('20'), setSelected('Sat'), setEnd('th')]}
                 />
                 <FAB
                     label='21'
                     style={styles.fab}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() =>[setSelectedDay('21'),setSelected('Sun'), setEnd('st')]}
                 />
                 <FAB
                     label='22'
                     style={styles.fab}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() =>[setSelectedDay('22'), setSelected('Mon'), setEnd('nd')]}
                 />
                 <FAB
                     label='23'
                     style={styles.fab}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() =>[setSelectedDay('23'), setSelected('Tue'), setEnd('rd')]}
                 />
             </View>
-            <List.Section title="19th Jul 2024, Fri">
+            <List.Section title={selectedDay+end+" Jul 2024, "+ selected}>
                 <List.Accordion
                     title="6:00 AM"
                     left={props => <List.Icon {...props} icon="calendar-multiple" />}>
-                    <List.Item title="Take Medicine1" description="1 Pill"
+                    <List.Item title="Take Medicine1" description="1 Pill" left={props => <IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>}
                         right={props => <Text><Checkbox /></Text>} />
 
-                    <List.Item title="Go for a walk" description="30 mins"
-                        right={props => <Text><Checkbox /></Text>} />
+                    <List.Item title="Go for a walk" description="30 mins"  left={props => <IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>}
+                        right={props => <Text><Checkbox status={checked ? 'checked' : 'unchecked'} onPress={() => {
+                            setChecked(!checked);
+                        }} /></Text>} />
                 </List.Accordion>
 
                 <List.Accordion
                     title="7:00 AM"
                     left={props => <List.Icon {...props} icon="calendar-multiple" />}>
-                    <List.Item title="First item" />
-                    <List.Item title="Second item" />
+                    <List.Item title="Breakfast" description="Oats and Milk"  left={props => <IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>}
+                        right={props => <Text><Checkbox status={checked ? 'checked' : 'unchecked'} onPress={() => {
+                            setChecked(!checked);
+                        }} /></Text>} />
+                    <List.Item title="Medicine 2" description="1 Pill"  left={props => <IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>}
+                        right={props => <Text><Checkbox status={checked ? 'checked' : 'unchecked'} onPress={() => {
+                            setChecked(!checked);
+                        }} /></Text>}
+                    />
                 </List.Accordion>
                 <List.Accordion
                     title="8:00 PM"
                     left={props => <List.Icon {...props} icon="calendar-multiple" />}>
-                    <List.Item title="First item" />
-                    <List.Item title="Second item" />
+                    <List.Item title="First item"  left={props =><IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>}
+                        right={props => <Text><Checkbox status={checked ? 'checked' : 'unchecked'} onPress={() => {
+                            setChecked(!checked);
+                        }} /></Text>} />
+                    <List.Item title="Second item" left={props => <IconButton  onPress={() => navigation.navigate('CreateNewSchedule')} icon="clipboard-edit-outline"/>} right={props => <Text><Checkbox status={checked ? 'checked' : 'unchecked'} onPress={() => {
+                        setChecked(!checked);
+                    }} /></Text>} />
                 </List.Accordion>
             </List.Section>
-            <Button style={{alignSelf:'flex-end', position:'absolute', bottom:40}} icon="plus" mode="elevated" onPress={() =>  navigation.navigate('CreateNewSchedule')}>
+            <Button style={{ alignSelf: 'flex-end', position: 'absolute', bottom: 40 }} icon="plus" mode="elevated" onPress={() => navigation.navigate('CreateNewSchedule')}>
                 Create New
             </Button>
 
